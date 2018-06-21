314611dfee5ddf0e1028fe96fd7c32f14d73a41e
Carlos Molina <molina.carlos.dev@gmail.com>
2018-06-20 20:14:35 -0500

    bamazonManager and Supervisor are now working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,27 +1,123 @@
-var products = require("./products.js");
-var inquirer = require("inquirer");
+let dotenv = require('dotenv').config();
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+let chalk = require('chalk');
+let keys = require('./keys.js');
 
-viewProducts(startShopping);
+let pool = mysql.createPool({
+	connectionLimit: 10,
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'bamazondb',
+});
 
-function startShopping() {
-    console.clear();
-    console.log("Let's shop");
-    inquirer.prompt([{
-        type: "input",
-        name: "itemID",
-        message: "Please type item id you want to purchase\n"
+let userCart = {
+	items: [],
+	qty: [],
+	total: 0,
+};
 
-    },
-    {
-        type: "input",
-        name: "itemQty",
-        message: "Please enter quantity of the  item id you want to purchase\n"
-    }]).then(function (answer) {
-        //console.log(answer.itemQty);
-        products.getProductDetail(answer.itemID, answer.itemQty);
-    });
+shop();
+function shop() {
+	pool.getConnection((err, connection) => {
+		if (err) throw err;
+		connection.query('SELECT * FROM bamazondb.products', function(error, results, fields) {
+			let productList = [];
+			results.forEach((value, index) => {
+				let str = `${value.item_id} ${value.product_name}                         `;
+				str = str.split('');
+				str.splice(25, 30, `$${value.price}`);
+				let strLayout = str.join('');
+				productList.push(strLayout);
+			});
+			connection.release();
+			if (error) throw error;
+
+			inquirer
+				.prompt([
+					{
+						type: 'list',
+						name: 'product_choice',
+						message:
+							'Welcome to bamazondb!\nWhich of the following products would you like to add to your cart?',
+						choices: productList,
+					},
+					{
+						type: 'input',
+						name: 'numItems',
+						message: 'How many would you like to add?',
+						validate: function(value) {
+							if (isNaN(value) === false) {
+								return true;
+							} else {
+								return false;
+							}
+						},
+					},
+				])
+				.then(answers => {
+					let idArr = answers.product_choice.split(' ');
+					let id = idArr[0];
+					connection.query('SELECT * FROM bamazondb.products WHERE item_id = ?;', [id], function(
+						error,
+						results,
+						fields
+					) {
+						if (results[0].stock_quantity >= answers.numItems) {
+							console.log(
+								chalk.blue(answers.product_choice),
+								chalk.yellow(` - quantity ${answers.numItems} has been added to your cart.`)
+							);
+							userCart.items.push(answers.product_choice);
+							userCart.qty.push(answers.numItems);
+							userCart.total += answers.numItems * results[0].price;
+							connection.query(
+								`UPDATE bamazondb.products SET stock_quantity = ${results[0].stock_quantity -
+									answers.numItems}, product_sales = ${results[0]
+									.product_sales} + ${answers.numItems} * ${results[0].price} WHERE item_id = ${id}`,
+								function(error, results, fields) {
+									if (error) throw error;
+								}
+							);
+
+							cartOrShop();
+						} else {
+							console.log(
+								chalk.red(
+									'Sorry, current stock is insufficient to complete your order. Please select another item or number of items.'
+								)
+							);
+							shop();
+						}
+
+						if (error) throw error;
+					});
+				});
+		});
+	});
 }
 
-function viewProducts(callback) {
-    products.getProducts(callback);
+function cartOrShop() {
+	inquirer
+		.prompt([
+			{
+				type: 'list',
+				name: 'doneShopping',
+				message: 'Would you like to continue shopping?',
+				choices: ['Continue shopping', 'View cart and checkout'],
+			},
+		])
+		.then(answers => {
+			if (answers.doneShopping === 'Continue shopping') {
+				shop();
+			} else {
+				userCart.items.forEach((value, index) => {
+					console.log(value);
+					console.log(`Qty: ${userCart.qty[index]}\n`);
+				});
+				console.log(chalk.green(`$${userCart.total}`) + ' (plus tax and shipping)');
+				process.exit();
+			}
+		});
 }
--- bamazonManager.js
+++ bamazonManager.js
@@ -1,7 +1,269 @@
-var exports = module.exports = {};
-var inquirer = require("inquirer");
-var mainMenu = require("./mainMenu.js");
-var methods = require("./bamazonMethods.js");
+let mysql = require('mysql');
+let inquirer = require('inquirer');
+let chalk = require('chalk');
+let Table = require('cli-table');
 
+let table;
+let createTable = function() {
+	table = new Table({
+		head: ['Item ID', 'Product Name', 'Product Description', 'Department Name', 'Price', 'Stock Quantity'],
+		colWidths: [9, 20, 70, 20, 10, 18],
+	});
+};
+createTable();
 
-mainMenu();
+let myValidation = function(value) {
+	if (isNaN(value) == false && value > 0) {
+		return true;
+	} else {
+		return false;
+	}
+};
+
+let connection = mysql.createConnection({
+	connectionLimit: 10,
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'bamazondb',
+});
+
+connection.connect(function(err) {
+	if (err) {
+		console.error('error connecting: ' + err.stack);
+		return;
+	}
+});
+
+manage();
+function manage() {
+	inquirer
+		.prompt([
+			{
+				type: 'list',
+				name: 'menu',
+				message: 'Menu Options:',
+				choices: [
+					'View Products for Sale',
+					'View Low Inventory',
+					'Add to Inventory',
+					'Add New Product',
+					'Remove Product',
+					'Quit',
+				],
+			},
+		])
+		.then(answers => {
+			connection.query('SELECT * FROM bamazondb.products', function(error, results, fields) {
+				if (error) throw error;
+				switch (answers.menu) {
+					case 'View Products for Sale':
+						let red;
+						results.forEach(value => {
+							if (value.stock_quantity < 5) {
+								red = chalk.red(value.stock_quantity);
+							} else {
+								red = value.stock_quantity;
+							}
+							table.push([
+								value.item_id,
+								value.product_name,
+								value.product_description,
+								value.department_name,
+								chalk.green(value.price),
+								red,
+							]);
+						});
+						console.log(table.toString());
+						createTable();
+						manage();
+						break;
+					case 'View Low Inventory':
+						connection.query('SELECT * FROM bamazondb.products WHERE stock_quantity<5;', function(
+							error,
+							results,
+							fields
+						) {
+							if (error) throw error;
+							results.forEach(value => {
+								table.push([
+									value.item_id,
+									value.product_name,
+									value.product_description,
+									value.department_name,
+									chalk.green(value.price),
+									chalk.red(value.stock_quantity),
+								]);
+							});
+							console.log(table.toString());
+							createTable();
+							manage();
+						});
+						break;
+					case 'Add to Inventory':
+						increaseInventory();
+						break;
+					case 'Add New Product':
+						addNewProduct();
+						break;
+					case 'Remove Product':
+						removeProduct();
+						break;
+					case 'Quit':
+						process.exit();
+				}
+			});
+		});
+}
+
+function increaseInventory() {
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				name: 'restockID',
+				message: 'Please enter a valid item ID:',
+				validate: myValidation,
+			},
+			{
+				type: 'input',
+				name: 'restockQty',
+				message: 'Please enter an integer to add to currently selected stock:',
+				validate: myValidation,
+			},
+		])
+		.then(answers => {
+			let stockID = parseInt(answers.restockID);
+			let stockQty = parseInt(answers.restockQty);
+			let exists = false;
+			connection.query('SELECT * FROM bamazondb.products WHERE item_id=?;', [stockID], function(
+				error,
+				results,
+				fields
+			) {
+				if (error) throw error;
+
+				if (results[0] !== undefined) {
+					exists = true;
+				}
+			});
+			connection.query(
+				'UPDATE bamazondb.products SET stock_quantity=stock_quantity + ? WHERE item_id=?;',
+				[stockQty, stockID],
+				function(error, results, fields) {
+					if (error) throw error;
+					if (exists) {
+						console.log(chalk.yellow('Stock updated successfully!'));
+						manage();
+					} else {
+						console.log(chalk.red('ID not found. Please try another ID.'));
+						invalidID();
+					}
+				}
+			);
+		});
+}
+
+function addNewProduct() {
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				name: 'name',
+				message: 'Please enter a product name:',
+			},
+			{
+				type: 'input',
+				name: 'description',
+				message: 'Please enter a product description:',
+			},
+			{
+				type: 'input',
+				name: 'department',
+				message: 'Please enter an item department:',
+			},
+			{
+				type: 'input',
+				name: 'price',
+				message: 'Please enter an item price:',
+				validate: myValidation,
+			},
+			{
+				type: 'input',
+				name: 'stock',
+				message: 'Please enter an initial stock quantity:',
+				validate: myValidation,
+			},
+		])
+		.then(answers => {
+			connection.query(
+				'INSERT INTO bamazondb.products (product_name, product_description, department_name, price, stock_quantity, product_sales) VALUES (?, ?, ?, ?, ?, 0);',
+				[answers.name, answers.description, answers.department, answers.price, answers.stock],
+				function(error, results, fields) {
+					if (error) throw error;
+					console.log(chalk.yellow('New product successfully added!'));
+					manage();
+				}
+			);
+		});
+}
+
+function invalidID() {
+	inquirer
+		.prompt([
+			{
+				type: 'list',
+				name: 'invalidIDnode',
+				message: 'Would you like to try another ID, or return to the main menu?',
+				choices: ['Try another ID', 'Return to main menu'],
+			},
+		])
+		.then(answers => {
+			if (answers.invalidIDnode === 'Try another ID') {
+				increaseInventory();
+			} else {
+				manage();
+			}
+		});
+}
+
+function removeProduct() {
+	inquirer
+		.prompt([
+			{
+				type: 'input',
+				name: 'idToRemove',
+				message: 'Please enter an ID of the product to DELETE:',
+				validate: myValidation,
+			},
+		])
+		.then(answers => {
+			let productID = parseInt(answers.idToRemove);
+			let exists = false;
+			connection.query('SELECT * FROM bamazondb.products WHERE item_id=?;', [productID], function(
+				error,
+				results,
+				fields
+			) {
+				if (error) throw error;
+
+				if (results[0] !== undefined) {
+					exists = true;
+				}
+			});
+			connection.query('DELETE FROM bamazondb.products WHERE item_id=?;', [productID], function(
+				error,
+				results,
+				fields
+			) {
+				if (error) throw error;
+				if (exists) {
+					console.log(chalk.yellow('Product deleted successfully!'));
+					manage();
+				} else {
+					console.log(chalk.red('ID not found. Please try another ID.'));
+					invalidID();
+				}
+			});
+		});
+}
